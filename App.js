5cca52b97025af502a34f84e97a9fd1dd4d6eaf6
Hadylfriaa <115735566+Hadylfriaa@users.noreply.github.com>
2023-05-22 15:25:32 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+//import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import Notification from './screens/Notification';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Notification" component={Notification} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCWs8Ry-GWulyNm-olmgG9-qIOdMLD9cNg",
+    authDomain: "app1-58471.firebaseapp.com",
+    projectId: "app1-58471",
+    storageBucket: "app1-58471.appspot.com",
+    messagingSenderId: "604333531028",
+    appId: "1:604333531028:web:8bf6ac32427333fbfb7981"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
